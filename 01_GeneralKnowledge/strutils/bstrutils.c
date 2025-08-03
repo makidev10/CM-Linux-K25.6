0611fb0a2deac2617c0edc62c4faa36869749712
dung.tran <dungta.dcdev10@gmail.com>
2025-08-03 21:49:06 +0700

    exercise1

---
+++ 01_GeneralKnowledge/strutils/bstrutils.c
@@ -0,0 +1,58 @@
+#include "strutils.h"
+#include <ctype.h>
+#include <string.h>
+
+char *str_reverse(char *str, size_t length) {
+  if (str == NULL)
+    return NULL;
+
+  for (int i = 0; i < length / 2; i++) {
+    char temp = str[i];
+    str[i] = str[length - i - 1];
+    str[length - i - 1] = temp;
+  }
+
+  return str;
+}
+
+char *str_trim(char *str, size_t length) {
+  if (str == NULL)
+    return NULL;
+
+  while (isspace((unsigned char)*str))
+    str++;
+
+  char *end = str + strlen(str) - 1;
+
+  while (end > str && isspace((unsigned char)*end))
+    end--;
+
+  *(end + 1) = '\0';
+
+  return str;
+}
+
+int str_to_int(char *str, size_t length) {
+  if (str == NULL)
+    return 0;
+
+  int result = 0;
+  int sign = 1;
+  int index = 0;
+  int temp = 0;
+  (void)str_trim(str, length);
+
+  if (str[0] == '-') {
+    sign = -1;
+    index++;
+  }
+
+  for (; index < length; index++) {
+    temp = str[index] - '0';
+    result = result * 10 + temp;
+  }
+
+  result = result * sign;
+
+  return result;
+}
---
+++ 01_GeneralKnowledge/strutils/strutils.h
@@ -0,0 +1,8 @@
+#ifndef _STRUTILS_H_
+#define _STRUTILS_H_
+
+char *str_reverse(char *str, size_t length);
+char *str_trim(char *str, size_t length);
+int str_to_int(char *str, size_t length);
+
+#endif //_STRUTILS_H_
